d737a5130115b1a858628f96b8c74c7212a494ad
diego-padron777 <ccool7344@gmail.com>
2026-03-24 21:39:12 -0600

    Unidad 1 Lección 3 Diego Padrón - 308169

---
+++ leccion3/src/main/java/com/example/leccion3/codigo_clase.kt
@@ -0,0 +1,12 @@
+package com.example.leccion3
+
+class Person(var fisrtName: String, val lastName: String){
+    val FullName: String
+        get(){
+            return "$fisrtName $lastName"
+        }
+}
+fun main(){
+    val person = Person("Samuel","Isaac")
+    println(person.FullName)
+}
---
+++ leccion4/src/main/java/com/example/leccion4/MyClass.kt
@@ -0,0 +1,4 @@
+package com.example.leccion4
+
+class MyClass {
+}
